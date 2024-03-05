3081e250721b3773a5331d922a1be6b21b061e3e
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2024-03-05 16:27:32 +0100

    Update books-API.js

--- src/js/books-API.js
+++ src/js/books-API.js
@@ -4,82 +4,112 @@ import { renderTopBooks, renderCategoriesList, renderBooksByCategory } from './r
 
 const BASE_URL = 'https://books-backend.p.goit.global';
 
-const fetchData = async (endpoint, params = null) => {
-  const fetchUrl = `${BASE_URL}/books${endpoint}`;
+//Fatch for top books
+let savedFetch = JSON.parse(sessionStorage.getItem('savedfetch')) || '';
+export const fetchGeneral = async booksPerRow => {
+
+  if(!savedFetch) {
+    const endpoint = '/books/top-books/';
+    const fetchUrl = BASE_URL + endpoint;
+  
+    try {
+      const response = await axios.get(fetchUrl);
+      sessionStorage.setItem('savedfetch', JSON.stringify(response.data));
+      savedFetch = response.data;
+      return renderTopBooks(response.data, booksPerRow);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    return renderTopBooks(savedFetch, booksPerRow);
+  }
+  
+};
+
+//Fatch for categories
+export const getCategoryList = async () => {
+  const endpoint = '/books/category-list/';
+  const fetchUrl = BASE_URL + endpoint;
+
+  try {
+    const response = await axios.get(fetchUrl);
+    return renderCategoriesList(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Fatch for categories books
+export const getBooksByCategory = async categoryName => {
+  const endpoint = '/books/category/';
+  const fetchUrl = BASE_URL + endpoint;
+  const params = {
+    category: categoryName,
+  };
 
   try {
     const response = await axios.get(fetchUrl, { params });
-    console.log('Data from server:', response.data)
+    return renderBooksByCategory(response.data, categoryName);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// повертає дані однієї книги по id
+export const getBookInfo = async id => {
+  const endpoint = `/books/${id}`;
+  const fetchUrl = BASE_URL + endpoint;
+
+  try {
+    const response = await axios.get(fetchUrl);
     return response.data;
   } catch (error) {
     console.log(error);
   }
 };
 
-export const getTopBooks = async (booksPerRow) => {
-  const data = await fetchData('/top-books/');
-  return renderTopBooks(data, booksPerRow);
-};
 
-export const getCategoryList = async () => {
-  const data = await fetchData('/category-list/');
-  return renderCategoriesList(data);
-};
-
-
-// export const getBooksByCategory = async (categoryName = '') => {
-//   const data = await fetchData('/category?category=', categoryName );
-//   return renderBooksByCategory(data);
-// };
-
-export const getBooksByCategory = async (categoryName = '') => {
-  const encodedCategoryName = encodeURIComponent(categoryName);
-  const endpoint = `/category?category=${encodedCategoryName}`;
-
-  const data = await fetchData(endpoint);
-  console.log('Category name3:', data);
-  // return renderBooksByCategory(categoryName, data);
-  const dataArray = Object.values(data); // Преобразование объекта в массив
-  return renderBooksByCategory(dataArray);
-};
-
-export const getBookInfo = async (id) => {
-  return fetchData(`/${id}`);
-};
-
-
-
-// import axios from "axios";
-// // import { renderTopBooks, renderCategoriesList, renderBooksByCategory } from './render-categories.js';
-
-// const BASE_URL = 'https://books-backend.p.goit.global/books';
 
 // const fetchData = async (endpoint, params = null) => {
+//   const fetchUrl = `${BASE_URL}/books${endpoint}`;
+
 //   try {
-//     const response = await axios.get(`${BASE_URL}${endpoint}`, { params });
+//     const response = await axios.get(fetchUrl, { params });
 //     console.log('Data from server:', response.data)
 //     return response.data;
 //   } catch (error) {
-//     console.error('Error fetching data:', error);
-//     throw error; // Пробрасываем ошибку дальше для обработки в вызывающем коде
+//     console.log(error);
 //   }
 // };
 
-// // Получение списка категорий
-// export const getCategoryList = async () => {
-//   return fetchData('/category-list');
+// export const getTopBooks = async (booksPerRow) => {
+//   const data = await fetchData('/top-books/');
+//   return renderTopBooks(data, booksPerRow);
 // };
 
-// // Получение списка топовых книг
-// export const getTopBooks = async () => {
-//   return fetchData('/top-books');
+// export const getCategoryList = async () => {
+//   const data = await fetchData('/category-list/');
+//   return renderCategoriesList(data);
 // };
-// // Получение книг по категории
+
+
+// // export const getBooksByCategory = async (categoryName = '') => {
+// //   const data = await fetchData('/category?category=', categoryName );
+// //   return renderBooksByCategory(data);
+// // };
+
 // export const getBooksByCategory = async (categoryName = '') => {
-//   return fetchData('/category', { category: categoryName });
+//   const encodedCategoryName = encodeURIComponent(categoryName);
+//   const endpoint = `/category?category=${encodedCategoryName}`;
+
+//   const data = await fetchData(endpoint);
+//   console.log('Category name3:', data);
+//   // return renderBooksByCategory(categoryName, data);
+//   const dataArray = Object.values(data); // Преобразование объекта в массив
+//   return renderBooksByCategory(dataArray);
 // };
-// // Получение информации о книге по ID
-// export const getBookById = async (id) => {
+
+// export const getBookInfo = async (id) => {
 //   return fetchData(`/${id}`);
 // };
 
