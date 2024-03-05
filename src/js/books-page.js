f1fa913161d0e5bff24e1e7f5aa8478c8fc9e90e
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2024-03-05 14:37:32 +0100

    Update books-page.js

--- src/js/books-page.js
+++ src/js/books-page.js
@@ -2,71 +2,107 @@
 import { getCategoryList, getTopBooks, getBooksByCategory } from './books-API.js';
 import { renderCategoriesList, renderTopBooks, renderBooksByCategory } from './render-categories.js';
 
-// Функция для отображения категорий
-async function displayCategories() {
-  try {
-    const categories = await getCategoryList();
-    const categoriesHTML = renderCategoriesList(categories);
-    const categoriesListContainer = document.querySelector('.categories-list');
-    categoriesListContainer.innerHTML = categoriesHTML;
-  } catch (error) {
-    console.error('Error displaying categories:', error);
-  }
-}
-
-// Функция для отображения топовых книг
-async function displayTopBooks() {
-  try {
-    const topBooks = await getTopBooks();
-    const topBooksHTML = renderTopBooks(topBooks);
-    const topBooksContainer = document.querySelector('.books-box');
-    topBooksContainer.innerHTML = topBooksHTML;
-  } catch (error) {
-    console.error('Error displaying top books:', error);
-  }
-}
-
-// Функция для отображения книг по категории
-async function displayBooksByCategory(categoryName) {
-  try {
-    const books = await getBooksByCategory(categoryName);
-    const booksHTML = renderBooksByCategory(categoryName, books);
-    const booksByCategoryContainer = document.querySelector('.books-box');
-    booksByCategoryContainer.innerHTML = booksHTML;
-  } catch (error) {
-    console.error('Error displaying books by category:', error);
-  }
-}
-
-// Инициализация страницы
-async function initializePage() {
-  try {
-    // Отображаем категории
-    await displayCategories();
-    // Отображаем топовые книги
-    await displayTopBooks();
-    // Здесь вы можете вызвать другие функции инициализации страницы, если это необходимо
-  } catch (error) {
-    console.error('Error initializing page:', error);
-  }
-}
-
-// Добавляем слушатель события для клика по категории
+const booksContainer = document.querySelector('.books-box');
 const categoriesListContainer = document.querySelector('.categories-list');
-categoriesListContainer.addEventListener('click', (event) => {
-  if (event.target.tagName === 'A') {
-    const categoryName = event.target.dataset.categorieName;
-    displayBooksByCategory(categoryName);
-  }
-});
 
-// Вызываем инициализацию страницы при загрузке документа
-document.addEventListener('DOMContentLoaded', initializePage);
+const BOOKS_PER_ROW_MAP = {
+    'default': 3,
+    'largeScreen': 5,
+    'smallScreen': 1
+};
 
+// Function to display books
+async function showBooks(renderedContent) {
+    booksContainer.innerHTML = renderedContent;
+    const titleElement = document.querySelector('.books-box-desc-title');
+    
+    if (titleElement) {
+        wrapLastWord(titleElement);
+    }
+}
+// Function to display categories
+async function showCategories() {
+    const renderedCategories = await getCategoryList();
+    categoriesListContainer.innerHTML = renderedCategories;
+}
 
+// Function to display top books
+export async function showTopBooks() {
+    const windowWidth = window.innerWidth;
+    const booksPerRow = determineBooksPerRow(windowWidth);
+    const renderedTop = await getTopBooks(booksPerRow);
+    showBooks(renderedTop);
+}
 
+// Function to display books by category
+export async function showBooksByCategory(categoryName) {
+    const renderedBooks = await getBooksByCategory(categoryName);
+    return showBooks(renderedBooks);
+}
+  
+  
+// Function to wrap the last word in the title
+function wrapLastWord(titleElement) {
+      const textContent = titleElement.textContent.split(" ");
+      const lastWord = textContent.pop();
+    const updatedContent = textContent.join(" ") + (textContent.length > 0 ? ` <span class="books-title-color">${lastWord}</span>` : lastWord);
+    titleElement.innerHTML = updatedContent;
+}
+// Function to determine the number of books per row based on window width
+function determineBooksPerRow(windowWidth) {
+    if (windowWidth >= 1440) {
+        return BOOKS_PER_ROW_MAP.largeScreen;
+    } else if (windowWidth < 768) {
+        return BOOKS_PER_ROW_MAP.smallScreen;
+    } else {
+        return BOOKS_PER_ROW_MAP.default;
+    }
+}
 
+// Initialize the page
+if (booksContainer) {
+    showTopBooks();
+    showCategories();
+    
+    categoriesListContainer.addEventListener('click', handleCategoryClick);
+    booksContainer.addEventListener('click', handleSeeMoreClick);
+}
 
+// Event handler for category click
+function handleCategoryClick(e) {
+    e.preventDefault();
+    const target = e.target;
+    
+    if (target.tagName === 'A') {
+        const categoryName = target.dataset.categoryName;
+        
+        categoriesListContainer.querySelector('.js-categories-current').classList.remove('js-categories-current');
+        target.classList.add('js-categories-current');
+
+        if (categoryName === '') {
+            // Якщо натиснуто "Усі категорії", показати TopBooks
+            showTopBooks();
+        } else {
+            // Якщо натиснуто конкретну категорію, показати книги з цієї категорії
+            showBooksByCategory(categoryName);
+        }
+    }
+}
+
+// Event handler for "See More" click
+function handleSeeMoreClick(e) {
+    e.preventDefault();
+    const target = e.target;
+    
+    if (target.classList.contains('books-btn-see-more')) {
+        const categoryName = target.dataset.categoryName;
+        
+        categoriesListContainer.querySelector('.js-categories-current').classList.remove('js-categories-current');
+        categoriesListContainer.querySelector(`[data-categoryName="${categoryName}"]`).classList.add('js-categories-current');
+        
+        showBooksByCategory(categoryName);
+    }
+}
 
 // import {
 //   getTopBooks,
