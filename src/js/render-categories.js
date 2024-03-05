300c4baa241fee45091d8392696b10cde316fa49
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2024-03-05 12:56:10 +0100

    Update render-categories.js

--- src/js/render-categories.js
+++ src/js/render-categories.js
@@ -1,65 +1,121 @@
+import { getTopBooks, getCategoryList, getBooksByCategory, getBookInfo } from './books-API.js';
 //Render one book
-function renderOneBook(books) {
-  const booksRendered = books
-    .map(el => {
-      const { _id, book_image, title, author } = el;
-      return `<li class="books-box-itm" id="${_id}">
-        <div class="books-box-wrap">
-                <img
-                  class="books-img"
-                  src="${book_image}"
-                  alt="${title}"
-                />
-                <div class="books-overlay">
-                  <a href="#" data-id="${_id}" class="books-overlay-text">
-                  quick view </a>
-                </div>
-              </div>
-              <div class="books-box-desc">
-                <p class="books-box-desc-title">${title}</p>
-                <p class="books-box-desc-author">${author}</p>
-              </div> 
-              </li>`;
-    })
-    .join('');
+export const renderOneBook = (book) => {
+  const { _id, title, author, book_image } = book;
+  return `
+    <li class="books-box-itm" id="${_id}">
+      <div class="books-box-wrap">
+        <img
+          class="books-img"
+          src="${book_image}"
+          alt="${title}"
+        />
+        <div class="books-overlay">
+          <a href="#" data-id="${_id}" class="books-overlay-text">quick view</a>
+        </div>
+      </div>
+      <div class="books-box-desc">
+        <p class="books-box-desc-title">${title}</p>
+        <p class="books-box-desc-author">${author}</p>
+      </div> 
+    </li>
+  `;
+};
 
-  return booksRendered;
-}
 //Render top books
-export const renderTopBooks = (data, booksPerRow) => {
-  const categoriesTopBooks = data
-    .map(el => {
-      const categorieName = el.list_name;
-      const books = renderOneBook(el.books.slice(0, booksPerRow));
-      return `<div class="books-box-holder">
-          <h3 class="books-box-subtitle">${categorieName}</h3>
-          <ul class="books-box-list">${books}</ul>
-          <button data-categorieName="${categorieName}" class="books-btn-see-more" type="button">see more</button>
-        </div>`;
-    })
-    .join('');
-
-  return `<div class="books-container"><h2 class="books-title">Best Sellers Books</h2>${categoriesTopBooks}</div>`;
+export const renderTopBooks = (categorieName, books) => {
+  return `
+    <div class="books-box-holder">
+      <h3 class="books-box-subtitle">${categorieName}</h3>
+      <ul class="books-box-list">${books}</ul>
+      <button data-categorieName="${categorieName}" class="books-btn-see-more" type="button">see more</button>
+    </div>
+  `;
 };
+
 //Render categories
-export const renderCategoriesList = data => {
-  const categoriesItems = data
-    .map(el => {
-      return `<li class="categories-itm"><a href="#" data-categorieName="${el.list_name}">${el.list_name}</a></li>`;
-    })
-    .join('');
+export const renderCategoriesList = (categories) => {
+  return categories.map((el) => `
+    <li class="categories-itm">
+      <a href="#" data-categorieName="${el.list_name}">${el.list_name}</a>
+    </li>
+  `).join('');
+};
 
-  return `<li class="categories-itm js-categories-current "><a href="#" data-categorieName="">All categories</a></li>${categoriesItems}`;
-};
 //Render category books
-export const renderBooksByCategory = (data, categoryName) => {
-  if (!categoryName) {
-    return '';
-  }
-  const books = renderOneBook(data);
-  const categoryMarkup = `<div class="books-category-box visually-hidden">
-  <h2 class="books-box-title">${categoryName}</h2>
-          <ul class="books-category-list">${books}</ul>
-          </div>`;
-  return categoryMarkup;
+export const renderBooksByCategory = (categoryName, books) => {
+  return `
+    <div class="books-category-box visually-hidden">
+      <h2 class="books-box-title">${categoryName}</h2>
+      <ul class="books-category-list">${books}</ul>
+    </div>
+  `;
 };
+
+
+
+//Render one book
+// function renderOneBook(books) {
+//   const booksRendered = books
+//     .map(el => {
+//       const { _id, book_image, title, author } = el;
+//       return `<li class="books-box-itm" id="${_id}">
+//         <div class="books-box-wrap">
+//                 <img
+//                   class="books-img"
+//                   src="${book_image}"
+//                   alt="${title}"
+//                 />
+//                 <div class="books-overlay">
+//                   <a href="#" data-id="${_id}" class="books-overlay-text">
+//                   quick view </a>
+//                 </div>
+//               </div>
+//               <div class="books-box-desc">
+//                 <p class="books-box-desc-title">${title}</p>
+//                 <p class="books-box-desc-author">${author}</p>
+//               </div> 
+//               </li>`;
+//     })
+//     .join('');
+
+//   return booksRendered;
+// }
+// //Render top books
+// export const renderTopBooks = (data, booksPerRow) => {
+//   const categoriesTopBooks = data
+//     .map(el => {
+//       const categorieName = el.list_name;
+//       const books = renderOneBook(el.books.slice(0, booksPerRow));
+//       return `<div class="books-box-holder">
+//           <h3 class="books-box-subtitle">${categorieName}</h3>
+//           <ul class="books-box-list">${books}</ul>
+//           <button data-categorieName="${categorieName}" class="books-btn-see-more" type="button">see more</button>
+//         </div>`;
+//     })
+//     .join('');
+
+//   return `<div class="books-container"><h2 class="books-title">Best Sellers Books</h2>${categoriesTopBooks}</div>`;
+// };
+// //Render categories
+// export const renderCategoriesList = data => {
+//   const categoriesItems = data
+//     .map(el => {
+//       return `<li class="categories-itm"><a href="#" data-categorieName="${el.list_name}">${el.list_name}</a></li>`;
+//     })
+//     .join('');
+
+//   return `<li class="categories-itm js-categories-current "><a href="#" data-categorieName="">All categories</a></li>${categoriesItems}`;
+// };
+// //Render category books
+// export const renderBooksByCategory = (data, categoryName) => {
+//   if (!categoryName) {
+//     return '';
+//   }
+//   const books = renderOneBook(data);
+//   const categoryMarkup = `<div class="books-category-box visually-hidden">
+//   <h2 class="books-box-title">${categoryName}</h2>
+//           <ul class="books-category-list">${books}</ul>
+//           </div>`;
+//   return categoryMarkup;
+// };
