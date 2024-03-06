e46b0c38744d62b99f4a063533c21a10596a6d70
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2024-03-06 13:08:24 +0000

    Deploying to gh-pages from @ anastasiiaipe/project-js-bookshelf@1d995833e213a5f53c8778eb7569edce4d1932ea 🚀

---
+++ assets/main-c9428b07.js
@@ -0,0 +1,26 @@
+import{i as S,a as g}from"./vendor-8cce9181.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))t(r);new MutationObserver(r=>{for(const n of r)if(n.type==="childList")for(const i of n.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&t(i)}).observe(document,{childList:!0,subtree:!0});function o(r){const n={};return r.integrity&&(n.integrity=r.integrity),r.referrerPolicy&&(n.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?n.credentials="include":r.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function t(r){if(r.ep)return;r.ep=!0;const n=o(r);fetch(r.href,n)}})();const f=document.querySelector(".loader");function a(){f.classList.remove("visually-hidden")}function l(){f.classList.add("visually-hidden")}a();window.addEventListener("load",()=>{setTimeout(()=>{l()},350)});function B(){window.scrollTo({top:0,behavior:"smooth"})}const c=document.querySelector(".top-btn");c.style.display="none";window.addEventListener("scroll",()=>{window.scrollY>window.innerHeight?c.style.display="inline-flex":c.style.display="none"});c.addEventListener("click",B);function m(e){return e.map(o=>{const{_id:t,book_image:r,title:n,author:i}=o;return`<li class="books-box-itm" id="${t}">
+      <div class="books-box-wrap">
+              <img
+                class="books-img"
+                src="${r}"
+                alt="${n}"
+              />
+              <div class="books-overlay">
+                <a href="#" data-id="${t}" class="books-overlay-text">
+                quick view </a>
+              </div>
+            </div>
+            <div class="books-box-desc">
+              <p class="books-box-desc-title">${n}</p>
+              <p class="books-box-desc-author">${i}</p>
+            </div> 
+            </li>`}).join("")}const C=(e,s)=>{if(!e||e.length===0){k("Sorry, books not found");return}else return`<h2 class="books-box-title">Best Sellers Books</h2>${e.map(t=>{const r=t.list_name,n=m(t.books.slice(0,s));return`<div class="books-box-holder">
+        <h3 class="books-box-subtitle">${r}</h3>
+         <ul class="books-box-list">${n}</ul>
+           <button data-categoryName="${r}" class="books-btn-see-more" type="button">see more</button>
+      </div>`}).join("")}`},T=e=>` <li class="categories-itm js-categories-current ">
+  <a href="#" data-categoryName="all categories">All categories</a></li>${e.sort((o,t)=>o.list_name<t.list_name?-1:o.list_name>t.list_name?1:0).map(o=>`<li class="categories-itm"><a href="#" data-categoryName="${o.list_name}">${o.list_name}</a></li>`).join("")}`,E=(e,s)=>{if(!e||e.length===0){k("Sorry, category of book not found");return}else{const o=m(e);return`<h2 class="books-box-title">${s}</h2>
+    <div class="books-category-box">
+      <ul class="books-category-list">${o}</ul>
+    </div>`}};function k(e){S.error({message:e,messageColor:"white",backgroundColor:"red",position:"topRight"})}const y="https://books-backend.p.goit.global",x=async e=>{const o=y+"/books/top-books/";try{const t=await g.get(o);return C(t.data,e)}catch(t){return console.error("Error fetching top books:",t),""}},$=async()=>{const s=y+"/books/category-list/";try{const o=await g.get(s);return T(o.data)}catch(o){console.error("Error fetching category list:",o)}},_=async e=>{const o=y+"/books/category/",t={category:e};try{const r=await g.get(o,{params:t});return E(r.data,e)}catch(r){console.error("Error fetching books by category:",r)}},d={default:3,largeScreen:5,smallScreen:1};function p(e){return e>=1440?d.largeScreen:e<768?d.smallScreen:d.default}const O=window.innerWidth;let u=p(O);window.addEventListener("resize",()=>{u=p(window.innerWidth)});async function b(){a();const e=document.querySelector(".categories-list"),s=await $();return e.innerHTML=s,e.addEventListener("click",q),l(),e}async function q(e){e.preventDefault();const s=e.target;if(!s.closest(".categories-itm"))return;const t=s.dataset.categoryname,r=await b();v(r,t),a(),t==="all categories"?w():await h(r,t)}document.addEventListener("click",P);async function P(e){if(e.target&&e.target.classList.contains("books-btn-see-more")){const s=e.target.dataset.categoryname;a(),await h(null,s),window.scrollTo({top:0,behavior:"smooth"});const o=await b();v(o,s)}}async function h(e,s){a();try{const o=document.querySelector(".books-box");if(!o){console.error("Element not found.");return}const t=await _(s);o.innerHTML=t,L(),console.log(t)}catch(o){console.error("Error displaying books by category:",o)}finally{l()}}async function w(){try{const e=document.querySelector(".books-box"),s=window.innerWidth;u=p(s);const t=await x(u);e.innerHTML=t,L()}catch(e){console.error("Error displaying top books:",e)}finally{l()}}window.addEventListener("load",()=>{w(),b()});function v(e,s){e.querySelectorAll(".categories-itm").forEach(t=>{t.firstElementChild.dataset.categoryname===s?t.classList.add("js-categories-current"):t.classList.remove("js-categories-current")})}async function L(){const e=document.querySelector(".books-box-title"),s=e.titleSpan.split(" "),o=s.pop(),t=s.join(" ")+(s.length>0?` <span class="books-title-span">${o}</span>`:o);e.innerHTML=t}
+//# sourceMappingURL=main-c9428b07.js.map
